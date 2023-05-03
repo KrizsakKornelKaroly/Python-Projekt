a1ae06062ff4ee35dc89f52c185090bb27bfa13c
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2023-05-03 12:27:03 +0200

    Design tesztterv

---
+++ Tervek/Sablonok/test.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from ttkthemes import ThemedTk
+ablak = ThemedTk('xpnative')
+ablak.configure(bg='#6A91EF')
+ablak.title('')
+afadrop = ["Alanyi adómentes", "Általános adózás", "Egyszerűsített vállalkozói adó", "ÁFA-kör - Csoportos adóalanyiság", "ÁFA-kör - Csoport közös adószáma"]
+ablak.mainloop() 
