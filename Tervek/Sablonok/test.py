02058619921d5714274e945698342c9af20c0124
KrizsakKornelKaroly <krizsakkornelkaroly@turr.hu>
2023-05-04 22:18:45 +0200

    új sablonok + néhány kép

--- Tervek/Sablonok/test.py
+++ Tervek/Sablonok/test.py
@@ -1,14 +1,14 @@
 from tkinter import *
-from ttkthemes import ThemedTk
 ablak = Tk()
 ablak.configure(bg='#5FC861')
 ablak.title('')
 ablak.geometry("600x300")
-afadrop = ["Alanyi adómentes", "Általános adózás", "Egyszerűsített vállalkozói adó", "ÁFA-kör - Csoportos adóalanyiság", "ÁFA-kör - Csoport közös adószáma"]
+afadrop = OptionMenu(ablak,"Alanyi adómentes", "Általános adózás", "Egyszerűsített vállalkozói adó", "ÁFA-kör - Csoportos adóalanyiság", "ÁFA-kör - Csoport közös adószáma")
+afadrop.pack()
 #tests = Label(ablak, text="Teszt", fg='white', bg='#5FC861')
-gomb1 = Button(ablak, text="Számítás",fg='white', bg='#359e18')
-gomb2 = Button(ablak, text="Számítás",fg='white', bg='#4162b0')
-gomb3 = Button(ablak, text="Számítás",fg='white', bg='#ad3e49')
+gomb1 = Button(ablak, text="Hozzáadás",fg='white', bg='#359e18')
+gomb2 = Button(ablak, text="Hozzáadás",fg='white', bg='#4162b0')
+gomb3 = Button(ablak, text="Hozzáadás",fg='white', bg='#ad3e49')
 gomb1.pack()
 gomb2.pack()
 gomb3.pack()
