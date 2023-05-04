96954440df00f2f2bf3281f0dac20e924f7cba62
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2023-05-04 11:12:53 +0200

    új hátterek

--- Tervek/Sablonok/test.py
+++ Tervek/Sablonok/test.py
@@ -1,7 +1,12 @@
 from tkinter import *
 from ttkthemes import ThemedTk
-ablak = ThemedTk('xpnative')
-ablak.configure(bg='#6A91EF')
+ablak = Tk()
+ablak.configure(bg='#5FC861')
 ablak.title('')
+ablak.geometry("600x300")
 afadrop = ["Alanyi adómentes", "Általános adózás", "Egyszerűsített vállalkozói adó", "ÁFA-kör - Csoportos adóalanyiság", "ÁFA-kör - Csoport közös adószáma"]
+tests = Label(ablak, text="Teszt", fg='white', bg='#5FC861')
+testg = Button(ablak, text="Teszt",fg='white', bg='#359e18')
+testg.pack()
+tests.pack()
 ablak.mainloop() 
