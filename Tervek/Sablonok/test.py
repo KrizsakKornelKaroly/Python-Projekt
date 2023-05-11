d34a2b3402fefb7e8248a4a87929ab5c3f666db1
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2023-05-11 11:29:55 +0200

    dropdown sablonok

--- Tervek/Sablonok/test.py
+++ Tervek/Sablonok/test.py
@@ -1,9 +1,23 @@
 from tkinter import *
+from tkinter import ttk
 ablak = Tk()
 ablak.configure(bg='#5FC861')
 ablak.title('')
 ablak.geometry("600x300")
-afadrop = OptionMenu(ablak,"Alanyi adómentes", "Általános adózás", "Egyszerűsített vállalkozói adó", "ÁFA-kör - Csoportos adóalanyiság", "ÁFA-kör - Csoport közös adószáma")
+#afadrop = OptionMenu(ablak,"Alanyi adómentes", "Általános adózás", "Egyszerűsített vállalkozói adó", "ÁFA-kör - Csoportos adóalanyiság", "ÁFA-kör - Csoport közös adószáma")
+#afadrop.pack()
+afadrop = ttk.Combobox(
+    state="readonly",
+    values=["Alanyi adómentes", "Általános adózás", "Egyszerűsített vállalkozói adó", "ÁFA-kör - Csoportos adóalanyiság", "ÁFA-kör - Csoport közös adószáma"],
+    width=30,
+)
+terulet = ttk.Combobox(
+    state="readonly",
+    values=["Baranya", "Bács-Kiskun", "Békés", "Borsod-Abaúj-Zemplén", "Csongrád-Csanád", "Fejér", "Győr-Moson-Sopron", "Hajdú-Bihar", "Heves", "Komárom-Esztergom", "Nógrád", "Pest", "Somogy", "Szabolcs-Szatmár-Bereg", "Jász-Nagykun-Szolnok", "Tolna", "Vas", "Veszprém", "Zala", "Észak-Budapest", "Kelet-Budapest", "Dél-Budapest", "Kiemelt Adózók Adóigazgatósága", "Kiemelt Ügyek Adóigazgatósága"],
+    width=30
+)
+terulet.pack()
+afadrop.config(background="#359e18")
 afadrop.pack()
 #tests = Label(ablak, text="Teszt", fg='white', bg='#5FC861')
 gomb1 = Button(ablak, text="Hozzáadás",fg='white', bg='#359e18')
