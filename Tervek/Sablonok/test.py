c96f33e05b2954baf50d9bddaeb2e8b29ef38ca5
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2023-05-24 12:30:00 +0200

    menü teszt

--- Tervek/Sablonok/test.py
+++ Tervek/Sablonok/test.py
@@ -1,11 +1,37 @@
 from tkinter import *
 from tkinter import ttk
 ablak = Tk()
-ablak.configure(bg='#5FC861')
+ablak.configure(bg='#eb942a')
 ablak.title('')
 ablak.geometry("600x300")
+menubar = Menu(ablak)
+file = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label ='Matematikai számítások', menu = file)
+file.add_command(label ='New File', command = None)
+file.add_command(label ='Open...', command = None)
+file.add_command(label ='Save', command = None)
+file.add_separator()
+file.add_command(label ='Exit', command = ablak.destroy)
+
+menubar.add_cascade(label ='Geometriai számítások', menu = file)
+file.add_command(label ='New File', command = None)
+file.add_command(label ='Open...', command = None)
+file.add_command(label ='Save', command = None)
+file.add_separator()
+file.add_command(label ='Exit', command = ablak.destroy)
+
+menubar.add_cascade(label ='Pénzügyi számítások', menu = file)
+file.add_command(label ='New File', command = None)
+file.add_command(label ='Open...', command = None)
+file.add_command(label ='Save', command = None)
+file.add_separator()
+file.add_command(label ='Exit', command = ablak.destroy)
+ablak.config(menu=menubar)
+
+
 #afadrop = OptionMenu(ablak,"Alanyi adómentes", "Általános adózás", "Egyszerűsített vállalkozói adó", "ÁFA-kör - Csoportos adóalanyiság", "ÁFA-kör - Csoport közös adószáma")
 #afadrop.pack()
+''' 
 afadrop = ttk.Combobox(
     state="readonly",
     values=["Alanyi adómentes", "Általános adózás", "Egyszerűsített vállalkozói adó", "ÁFA-kör - Csoportos adóalanyiság", "ÁFA-kör - Csoport közös adószáma"],
@@ -25,7 +51,8 @@ gomb2 = Button(ablak, text="Hozzáadás",fg='white', bg='#4162b0')
 gomb3 = Button(ablak, text="Hozzáadás",fg='white', bg='#ad3e49')
 gomb1.pack()
 gomb2.pack()
-gomb3.pack()
+gomb3.pack() 
+'''
 
 #testm = Entry(ablak, fg='white', bg='#4162b0')
 #testm.pack()
